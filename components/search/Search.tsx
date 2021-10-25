d00ab1d35360924424527d397d236d6a10ab6f15
hans <hans.krebs@me.com>
2021-10-25 20:52:39 +0200

    Implement search.

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -1,5 +1,7 @@
 import theme from '@styles/theme';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { CompaniesContext } from 'contexts';
 
 const Input = styled.input`
   border-radius: ${theme.borderRadius};
@@ -10,12 +12,27 @@ const Input = styled.input`
 `;
 
 export default function Search() {
+  const companiesContext = useContext(CompaniesContext);
+  const [query, setQuery] = useState('');
+
+  function handleChange() {
+    return (e: React.FormEvent<HTMLInputElement>) =>
+      setQuery(e.currentTarget.value);
+  }
+
+  useEffect(() => {
+    companiesContext.dispatch({ type: 'searchFilter', query });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
   return (
     <Input
       type='search'
       name='search'
       id='search'
       placeholder='Company name ...'
+      value={query}
+      onChange={handleChange()}
     />
   );
 }
