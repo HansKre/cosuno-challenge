a9307091c4efa7af09fe0be9080adae5890bc891
hans <hans.krebs@me.com>
2021-10-25 16:08:32 +0200

    Mock API response.

---
+++ pages/api/companies.ts
@@ -0,0 +1,20 @@
+import { ICompany } from 'types';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { companies } from './fixture';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ICompany[]>
+) {
+  if (req.method === 'GET') {
+    if (companies && companies.length > 0) {
+      res.status(200).json(companies);
+    } else {
+      // Internal Server Error
+      res.status(500).end();
+    }
+  } else {
+    // Method Not Allowed
+    res.status(405).end();
+  }
+}
---
+++ pages/api/fixture.ts
@@ -0,0 +1,59 @@
+import { ICompany } from 'types';
+
+export const companies: ICompany[] = [
+  {
+    company: 'Cosuno GmbH',
+    location: 'Berlin',
+    specialties: ['Excavation', 'Plumbing', 'Electrical'],
+  },
+  {
+    company: 'Lowe GmbH',
+    location: 'Stuttgart',
+    specialties: ['Sanitary', 'Electrical'],
+  },
+  {
+    company: 'Medhurst GmbH',
+    location: 'Berlin',
+    specialties: ['Excavation', 'Gardening'],
+  },
+  {
+    company: 'Renard GmbH',
+    location: 'Köln',
+    specialties: ['Excavation', 'Plumbing', 'Electrical'],
+  },
+  {
+    company: 'Leclerc GmbH',
+    location: 'Düsseldorf',
+    specialties: ['Electrical'],
+  },
+  {
+    company: 'Riviere GmbH',
+    location: 'München',
+    specialties: ['Internet', 'Electrical'],
+  },
+  {
+    company: 'Vasseur GmbH',
+    location: 'München',
+    specialties: ['Excavation', 'Plumbing', 'Electrical'],
+  },
+  {
+    company: 'Bourgeois GmbH',
+    location: 'Dortmund',
+    specialties: ['Heating', 'Plumbing'],
+  },
+  {
+    company: 'Renault GmbH',
+    location: 'Leipzig',
+    specialties: ['Plumbing'],
+  },
+  {
+    company: 'Pierre GmbH',
+    location: 'Köln',
+    specialties: ['Tiler', 'Sanitary'],
+  },
+  {
+    company: 'Le roux GmbH',
+    location: 'Berlin',
+    specialties: ['Tiler', 'Plumbing'],
+  },
+];
