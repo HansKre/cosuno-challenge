b4f40fa81e43490294e27e9a2477eee0b86418ae
hans <hans.krebs@me.com>
2021-10-26 08:34:24 +0200

    Create separate ModalDialog component.

--- components/filter/Filter.tsx
+++ components/filter/Filter.tsx
@@ -1,32 +1,17 @@
-import CompaniesContext from '@contextscompanies';
-import React, { useContext, useState } from 'react';
-import { Blur } from './Blur';
-import Checkbox from './Checkbox';
-import { Column } from './Column';
-import { DialogTitle } from './DialogTitle';
+import { useContext, useState } from 'react';
+import CompaniesContext from 'contexts/companies';
 import { FilterIconActive, FilterIcon } from './Icons';
-import { Modal } from './Modal';
-import { Button } from './Button';
+import ModalDialog from './ModalDialog';
 
 export default function Filter() {
   const companiesContext = useContext(CompaniesContext);
-  const { companies, specialtiesFilter } = companiesContext.state;
-  const { dispatch } = companiesContext;
+  const { specialtiesFilter } = companiesContext.state;
 
   const [visible, setVisible] = useState(false);
   function handleClick() {
     setVisible((prev) => !prev);
   }
-  function handleChange(
-    e: React.FormEvent<HTMLInputElement>,
-    specialty: string
-  ) {
-    if (e.currentTarget.checked) {
-      dispatch({ type: 'addSpecialty', specialty: specialty });
-    } else {
-      dispatch({ type: 'removeSpecialty', specialty: specialty });
-    }
-  }
+
   return (
     <>
       {specialtiesFilter.length > 0 ? (
@@ -34,34 +19,7 @@ export default function Filter() {
       ) : (
         <FilterIcon onClick={handleClick} style={{ fontSize: '2rem' }} />
       )}
-      {visible && (
-        <>
-          <Blur onClick={handleClick} />
-          <Modal>
-            <Column>
-              <DialogTitle>Filter</DialogTitle>
-              {companies &&
-                companies
-                  .reduce<string[]>((uniqueSpecialties, company) => {
-                    company.specialties.forEach((specialty) => {
-                      if (!uniqueSpecialties.includes(specialty))
-                        uniqueSpecialties.push(specialty);
-                    });
-                    return uniqueSpecialties;
-                  }, [])
-                  .map((specialty) => (
-                    <Checkbox
-                      key={specialty}
-                      label={specialty}
-                      checked={specialtiesFilter.includes(specialty)}
-                      onChange={(e) => handleChange(e, specialty)}
-                    />
-                  ))}
-              <Button onClick={handleClick}>Ok</Button>
-            </Column>
-          </Modal>
-        </>
-      )}
+      <ModalDialog visible={visible} onClick={handleClick} />
     </>
   );
 }
---
+++ components/filter/ModalDialog.tsx
@@ -0,0 +1,60 @@
+import { useContext } from 'react';
+import CompaniesContext from 'contexts/companies';
+import { Blur } from './Blur';
+import Checkbox from './Checkbox';
+import { Column } from './Column';
+import { DialogTitle } from './DialogTitle';
+import { Modal } from './Modal';
+import { Button } from './Button';
+
+interface IProps {
+  visible: boolean;
+  onClick: () => void;
+}
+export default function ModalDialog({ visible, onClick }: IProps) {
+  const companiesContext = useContext(CompaniesContext);
+  const { companies, specialtiesFilter } = companiesContext.state;
+  const { dispatch } = companiesContext;
+
+  function handleChange(
+    e: React.FormEvent<HTMLInputElement>,
+    specialty: string
+  ) {
+    if (e.currentTarget.checked) {
+      dispatch({ type: 'addSpecialty', specialty: specialty });
+    } else {
+      dispatch({ type: 'removeSpecialty', specialty: specialty });
+    }
+  }
+
+  return visible ? (
+    <>
+      <Blur onClick={onClick} />
+      <Modal>
+        <Column>
+          <DialogTitle>Filter</DialogTitle>
+          {companies &&
+            companies
+              .reduce<string[]>((uniqueSpecialties, company) => {
+                company.specialties.forEach((specialty) => {
+                  if (!uniqueSpecialties.includes(specialty))
+                    uniqueSpecialties.push(specialty);
+                });
+                return uniqueSpecialties;
+              }, [])
+              .map((specialty) => (
+                <Checkbox
+                  key={specialty}
+                  label={specialty}
+                  checked={specialtiesFilter.includes(specialty)}
+                  onChange={(e) => handleChange(e, specialty)}
+                />
+              ))}
+          <Button onClick={onClick}>Ok</Button>
+        </Column>
+      </Modal>
+    </>
+  ) : (
+    <></>
+  );
+}
